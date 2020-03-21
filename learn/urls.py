df155fa9804d13fef9d3686bb7606b0ba9c07c4e
rokong <hongrr123@gmail.com>
2020-03-21 08:33:34 +0000

    listview of weeklyclasses

--- learn/urls.py
+++ learn/urls.py
@@ -4,6 +4,10 @@ from . import views
 
 # namespace for url
 app_name = 'learn'
-urlpatterns = [    
+urlpatterns = [
     path('', views.classrooms.as_view(), name='classrooms'),
+    
+    path('<int:classroom>/', views.weeklyclasses.as_view(), name='weeklyclasses'),
+    
+    path('chapter/', views.chapter.as_view(), name='chapter'),
 ]
--- learn/views.py
+++ learn/views.py
@@ -1,11 +1,23 @@
-from django.shortcuts import render, resolve_url, redirect
-from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-#from django import forms
+from django.shortcuts import render, resolve_url, redirect, get_object_or_404
+from django.views.generic import ListView, TemplateView
 
 from learn.models import *
 
 class classrooms(ListView):
     model = ClassRoom
     template_name = 'learn/classrooms.html'
+
+class weeklyclasses(ListView):
+    model = WeeklyClass
+    template_name = 'learn/weeklyclasses.html'
+
+    def get_queryset(self):
+        return WeeklyClass.objects.filter(classroom__pk=self.kwargs['classroom']).order_by('num')
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['classroom'] = get_object_or_404(ClassRoom, pk=self.kwargs['classroom'])
+        return context
+
+class chapter(TemplateView):
+    template_name = 'learn/chapter.html'
